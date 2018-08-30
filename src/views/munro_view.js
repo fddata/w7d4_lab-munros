cfbb2dff2cd11732a5722a5d711d4e87e5398dc1
fddata <fraserjdouglas@gmail.com>
2018-08-30 15:04:30 +0100

    adds list to MunroView

--- src/views/munro_view.js
+++ src/views/munro_view.js
@@ -10,6 +10,9 @@ MunroView.prototype.render = function (){
   const name = this.createMunroHeading();
   munrosContainer.appendChild(name);
 
+  const list = this.createMunroList();
+  munrosContainer.appendChild(list);
+
   this.munrosContainer.appendChild(munrosContainer);
 
 };
@@ -27,6 +30,27 @@ MunroView.prototype.createMunroHeading = function () {
   }
   return name;
 };
+
+
+MunroView.prototype.createMunroList = function () {
+  const munroList = document.createElement('ul');
+  munroList.classList.add('munro-list');
+  this.populateList(munroList);
+  return munroList;
+};
+
+
+MunroView.prototype.populateList = function (list) {
+const munroListItemMeaning = document.createElement('li');
+munroListItemMeaning.textContent = `Meaning: "${this.munro.meaning}"`;
+
+const munroListItemHeight = document.createElement('li');
+munroListItemHeight.textContent = `Height: ${this.munro.height}m`;
+
+list.appendChild(munroListItemMeaning);
+list.appendChild(munroListItemHeight);
+};
+
 
 
 module.exports = MunroView;
