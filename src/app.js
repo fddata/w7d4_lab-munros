0363229cd10ba91a45e2550dcd0a7ed67f257eb7
fddata <fraserjdouglas@gmail.com>
2018-08-30 14:08:03 +0100

    adds getData to Munros and pulls in all API data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+const Munros = require('./models/munros.js');
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
-})
+
+
+const munros = new Munros();
+munros.getData();
+
+});
